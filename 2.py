dc0f57bad3682b4ec1f489be42c69f0a9f64b0c8
Michael Burke <michael.dburke56@gmail.com>
2022-04-19 20:12:30 -0400

    Resolve challenge 2.

---
+++ 2.py
@@ -0,0 +1,9 @@
+## Modify the name on Dot’s passport so it can be recognized by passport control. Which combination of string functions will give us the right results?
+
+## name: Dot
+
+## Required format: #DOT!
+
+name = ' Dot '
+
+print(f'#{name.strip().upper()}!')
